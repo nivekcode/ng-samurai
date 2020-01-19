4de9eb6e3f1d3de357174f39f0822ba2d184baf1
kreuzerk <kevin.kreuzer90@icloud.com>
2020-01-19 19:12:14 +0100

    chore(lib): adjust library

---
+++ lib-sample/projects/lib-sample/src/lib/time/howdy-time.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {HowdyTimeComponent} from './howdy-time.component';
+
+@NgModule({
+  declarations: [HowdyTimeComponent],
+  exports: [HowdyTimeComponent]
+})
+export class HowdyTimeModule {
+}
