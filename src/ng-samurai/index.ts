7dfc17f0381540246eccec5de17a1fae9af0278e
kreuzerk <kevin.kreuzer90@icloud.com>
2020-01-19 18:44:22 +0100

    feat(api): generate main public API

--- src/ng-samurai/index.ts
+++ src/ng-samurai/index.ts
@@ -4,36 +4,57 @@ import {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
 // per file.
 export function ngSamurai(_options: any): Rule {
     return (tree: Tree, _context: SchematicContext) => {
+        const relativeLibraryPaths: string[] = [];
+
         tree.getDir('./lib-sample/projects/lib-sample/src/lib').visit((filePath) => {
             if (filePath.endsWith('module.ts')) {
                 const fileDirectoryPath = getFileDirectoryPath(filePath);
                 const moduleName = getModuleName(filePath);
+                relativeLibraryPaths.push(getRelativeLibraryPath(filePath));
 
                 tree.create(`${fileDirectoryPath}/index.ts`, createIndexFileContent());
                 tree.create(`${fileDirectoryPath}/package.json`, createPackageJSONContent());
-                tree.create(`${fileDirectoryPath}/public_api.ts`, createPublicAPI(moduleName));
+                tree.create(`${fileDirectoryPath}/public-api.ts`, createPublicAPI(moduleName));
             }
         });
+
+        tree.overwrite('./lib-sample/projects/lib-sample/src/public-api.ts', createMainPublicAPIContent(relativeLibraryPaths));
+
         return tree;
     };
 }
+
+const getRelativeLibraryPath = (filePath: string): string => {
+    const libpath = filePath.split('projects/')[1];
+    return libpath.substring(0, libpath.lastIndexOf('/'));
+};
+
 
 const getFileDirectoryPath = (filePath: string) => filePath.substring(0, filePath.lastIndexOf('/'));
 
 const getModuleName = (filePath: string) => filePath.substring(filePath.lastIndexOf('/') + 1, filePath.lastIndexOf('.'));
 
 const createIndexFileContent = (): string => {
-    return `export * from './public_api;'`
+    return `export * from './public-api;'`
 };
 
 const createPackageJSONContent = (): string => {
     return `{
         "ngPackage": {
             "lib": {
-                "entryFile": "public_api.ts"
+                "entryFile": "public-api.ts"
             }
         }
     }`
+};
+
+const createMainPublicAPIContent = (libariePaths: string[]): string => {
+    let content = '';
+    libariePaths.forEach((path: string) => {
+        content += `export * from ${path};
+        `;
+    });
+    return content;
 };
 
 const createPublicAPI = (moduleName: string): string => {
