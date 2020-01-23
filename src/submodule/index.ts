5fa5c73553ecbd3c5846b5d4850b46b06e3b5273
kreuzerk <kevin.kreuzer90@icloud.com>
2020-01-23 18:59:02 +0100

    chore(submodule): fix wrong import

--- src/submodule/index.ts
+++ src/submodule/index.ts
@@ -1,77 +1,77 @@
 import {
-  url,
-  move,
-  apply,
-  chain,
-  template,
-  mergeWith,
-  externalSchematic,
-  Tree,
-  Rule,
-  SchematicContext,
-  SchematicsException
+    url,
+    move,
+    apply,
+    chain,
+    template,
+    mergeWith,
+    externalSchematic,
+    Tree,
+    Rule,
+    SchematicContext,
+    SchematicsException
 } from '@angular-devkit/schematics';
-import { strings } from '@angular-devkit/core';
-import { parseName } from '@schematics/angular/utility/parse-name';
+import {strings} from '@angular-devkit/core';
+import {parseName} from '@schematics/angular/utility/parse-name';
 
-import { Schema as SubmoduleOptions } from './schema';
+import {Schema as SubmoduleOptions} from './schema.model';
 
 export function submodule(_options: SubmoduleOptions): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    const moduleSchematicsOptions = { ..._options };
-    const componentSchematicsOptions = { ..._options };
+    return (tree: Tree, _context: SchematicContext) => {
+        const moduleSchematicsOptions = {..._options};
+        const componentSchematicsOptions = {..._options};
 
-    const workspaceAsBuffer = tree.read('angular.json');
-    if (!workspaceAsBuffer) {
-      throw new SchematicsException('Not and Angular CLI workspace');
-    }
+        const workspaceAsBuffer = tree.read('angular.json');
+        if (!workspaceAsBuffer) {
+            throw new SchematicsException('Not and Angular CLI workspace');
+        }
 
-    const workspace = JSON.parse(workspaceAsBuffer.toString());
+        const workspace = JSON.parse(workspaceAsBuffer.toString());
 
-    const projectName = _options.project || workspace.defaultProject;
-    const project = workspace.projects[projectName];
+        const projectName = _options.project || workspace.defaultProject;
+        const project = workspace.projects[projectName];
 
-    if (project.projectType === 'application') {
-      throw new SchematicsException(
-        'The "submodule" schematics works only for the "library" projects, please specify correct project using --project flag'
-      );
-    }
+        if (project.projectType === 'application') {
+            throw new SchematicsException(
+                'The "submodule" schematics works only for the "library" projects, please specify correct project using --project flag'
+            );
+        }
 
-    const path = _options.path || `${project.sourceRoot}/lib`;
+        const path = _options.path || `${project.sourceRoot}/lib`;
 
-    const parsed = parseName(path, _options.name);
+        const parsed = parseName(path, _options.name);
 
-    _options.name = parsed.name;
+        _options.name = parsed.name;
 
-    const sourceTemplate = url('./files');
-    const sourceTemplateParametrized = apply(sourceTemplate, [
-      template({
-        ..._options,
-        ...strings
-      }),
-      move(parsed.path)
-    ]);
+        const sourceTemplate = url('./files');
+        const sourceTemplateParametrized = apply(sourceTemplate, [
+            template({
+                ..._options,
+                ...strings
+            }),
+            move(parsed.path)
+        ]);
 
-    const rules = [mergeWith(sourceTemplateParametrized)];
+        const rules = [mergeWith(sourceTemplateParametrized)];
 
-    if (_options.generateModule || _options.generateComponent) {
-      rules.push(
-        externalSchematic('@schematics/angular', 'module', {
-          ...moduleSchematicsOptions,
-          project: projectName
-        })
-      );
-    }
+        if (_options.generateModule || _options.generateComponent) {
+            rules.push(
+                externalSchematic('@schematics/angular', 'module', {
+                    ...moduleSchematicsOptions,
+                    project: projectName
+                })
+            );
+        }
 
-    if (_options.generateComponent) {
-      rules.push(
-        externalSchematic('@schematics/angular', 'component', {
-          ...componentSchematicsOptions,
-          project: projectName
-        })
-      );
-    }
+        if (_options.generateComponent) {
+            rules.push(
+                externalSchematic('@schematics/angular', 'component', {
+                    ...componentSchematicsOptions,
+                    project: projectName
+                })
+            );
+        }
 
-    return chain(rules);
-  };
+        return chain(rules);
+    };
 }
