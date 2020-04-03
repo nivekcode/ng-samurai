ef15b48b764a9e17bdc40c26e615155d2fb5105a
kreuzerk <kevin.kreuzer90@icloud.com>
2020-04-03 09:22:50 +0200

    chore(modifypath): refactor modify path

--- src/rules/update-import-paths.rule.ts
+++ src/rules/update-import-paths.rule.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as ts from 'typescript';
 import { Tree } from '@angular-devkit/schematics';
 import { findModule } from '@schematics/angular/utility/find-module';
+import { convertToAbsolutPath, getFolderPath } from '../shared/pathHelper';
 
 interface Modification {
   startPosition: number;
@@ -10,90 +11,53 @@ interface Modification {
 }
 
 export function updateImportPaths(tree: Tree, filePath: string): string {
-  const moduleOfFile = findModule(
-    tree,
-    filePath.substring(0, filePath.lastIndexOf('/'))
-  );
   const sourceCode = fs.readFileSync(filePath, 'utf-8');
-  const sourceFile = ts.createSourceFile(
-    filePath,
-    sourceCode,
-    ts.ScriptTarget.Latest,
-    true
-  );
-  const modifications = modify(sourceFile, moduleOfFile, filePath, tree);
+  const sourceFile = ts.createSourceFile(filePath, sourceCode, ts.ScriptTarget.Latest, true);
+  const modifications = modify(sourceFile, filePath, tree);
   return applyReplacements(sourceCode, modifications);
 }
 
-function modify(
-  node: ts.Node,
-  moduleOfFile: string,
-  filePath: string,
-  tree: Tree
-): Modification[] {
+function modify(node: ts.Node, filePath: string, tree: Tree): Modification[] {
   const modifications: Modification[] = [];
+  const moduleOfFile = findModule(tree, getFolderPath(filePath));
 
   function updatePaths(node: ts.Node) {
     if (ts.isImportDeclaration(node)) {
       const importSegments = node.getChildren();
-      const importStringLiteral =
-        importSegments.find(
-          segment => segment.kind === ts.SyntaxKind.StringLiteral
-        ) || '';
+      const importNode = importSegments.find(
+        segment => segment.kind === ts.SyntaxKind.StringLiteral
+      );
 
       if (
-        importStringLiteral &&
-        !isThirdPartyLibImport(importStringLiteral)
-        && importsForeignModuleCode(importStringLiteral, moduleOfFile, filePath, tree)
+        importNode &&
+        !isThirdPartyLibImport(importNode) &&
+        importsForeignModuleCode(importNode, moduleOfFile, filePath, tree)
       ) {
-        console.log('Bluberbliblu');
         modifications.push({
-          startPosition: importStringLiteral.pos + 1,
-          endPosition: importStringLiteral.end + 1,
+          startPosition: importNode.pos + 1,
+          endPosition: importNode.end + 1,
           content: `'foo';`
         });
       }
     }
   }
+
   node.forEachChild(updatePaths);
   return modifications;
 }
 
-function isThirdPartyLibImport(importStringLiteral: ts.Node): boolean {
-  return !importStringLiteral.getText().startsWith(`'.`);
+function isThirdPartyLibImport(importNode: ts.Node): boolean {
+  return !importNode.getText().startsWith(`'.`);
 }
 
 function importsForeignModuleCode(
-  importStringLiteral: ts.Node,
+  importNode: ts.Node,
   fileModulePath: string,
   filePath: string,
   tree: Tree
 ): boolean {
-  const text = importStringLiteral.getText();
-  if (!text) {
-    return false;
-  }
-
-  const numberOfDots = (text as any).match(/[^a-zA-Z0-9]*/)[0].match(/\./g)
-    ?.length;
-  const levels = Math.floor(numberOfDots / 2);
-  const splittedPath = filePath.split('/');
-  const folderPath = splittedPath
-    .slice(0, splittedPath.length - levels - 1)
-    .join('/');
-
-  const secondpartOfPath = (text as any).match(/\/[a-zA-Z0-9].*/);
-  if (!secondpartOfPath) {
-    return false;
-  }
-  if (!folderPath || levels === 0) {
-    return false;
-  }
-  // import {HowdyTimeModule} from 'lib-sample/src/lib/time';
-  console.log('Folderpath', folderPath);
-  console.log('Scondpart', secondpartOfPath);
-  const modulePath = findModule(tree, `${folderPath}/${secondpartOfPath}`);
-  console.log('New Importpath: ', convertModulePathToPublicAPIImport(modulePath));
+  const importStringLiteral = importNode.getText();
+  const modulePath = findModule(tree, convertToAbsolutPath(filePath, importStringLiteral));
   return fileModulePath !== modulePath;
 }
 
@@ -103,10 +67,7 @@ const convertModulePathToPublicAPIImport = (modulePath: string): string => {
   return pathSegments && pathSegments.length ? pathSegments[1] : '';
 };
 
-function applyReplacements(
-  source: string,
-  modifications: Modification[]
-): string {
+function applyReplacements(source: string, modifications: Modification[]): string {
   for (const modification of modifications.reverse()) {
     source =
       source.slice(0, modification.startPosition) +
--- src/shared/pathHelper.ts
+++ src/shared/pathHelper.ts
@@ -18,3 +18,19 @@ export const getSourceRootPath = (tree: Tree, projectName?: string): string => {
 };
 
 export const getLibRootPath = (tree: Tree, projectName?: string): string => `${getSourceRootPath(tree, projectName)}/lib`;
+
+export const getFolderPath = (filePath: string) => filePath.substring(0, filePath.lastIndexOf('/'));
+
+export const convertToAbsolutPath = (filePath: string, importStringLiteral: string): string => {
+    const levelsUp = getLevels(importStringLiteral);
+    const filePathSegments = filePath.split('/');
+    const folderPathAfterLevelsMove = filePathSegments.slice(0, filePathSegments.length - levelsUp - 1).join('/');
+    const pathAfterRelativeSegment = importStringLiteral.match(/\/[a-zA-Z0-9].*/);
+    return `${folderPathAfterLevelsMove}${pathAfterRelativeSegment}`;
+};
+
+function getLevels(importStringLiteral: string): number {
+    const numberOfDots = importStringLiteral.match(/[^a-zA-Z0-9]*/)[0].match(/\./g)?.length;
+    return Math.floor(numberOfDots / 2);
+}
+
